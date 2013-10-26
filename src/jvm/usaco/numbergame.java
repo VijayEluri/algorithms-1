0780e0c7b0afad48d4d02adb3b4838f0c843276c
rdsr <rdsr@yahoo-inc.com>
2013-10-26 23:54:21 +0530

    subset sums

--- src/jvm/usaco/numbergame.java
+++ src/jvm/usaco/numbergame.java
@@ -13,20 +13,32 @@ public class numbergame {
     public numbergame(int[] seq, int n) {
         this.n = n;
         this.seq = seq;
-        this.score = new int[n][n]; 
+        this.score = new int[n][n];
         this.sum = new int[n][n];
 
         sum[0][0] = seq[0];
         for (int i = 1; i < n; i++) {
             sum[i][i] = seq[i];
-            sum[0][i] = seq[i] + sum[0][i-1];
+            sum[0][i] = seq[i] + sum[0][i - 1];
         }
-        
+
         for (int i = 1; i < n; i++) {
             for (int j = i; j < n; j++) {
-                sum[i][j] = sum[0][j] - sum[0][i-1];
+                sum[i][j] = sum[0][j] - sum[0][i - 1];
             }
         }
+    }
+
+    private int solve(int i, int j) {
+        if (score[i][j] != 0) {
+            return score[i][j];
+        }
+        if (i == j) {
+            return score[i][j] = seq[i];
+        }
+        final int p2ls = solve(i + 1, j);
+        final int p2rs = solve(i, j - 1);
+        return score[i][j] = sum[i][j] - Math.min(p2ls, p2rs);
     }
 
     public static void main(String[] args) throws IOException {
@@ -40,24 +52,5 @@ public class numbergame {
 
         final numbergame ng = new numbergame(seq, n);
         System.out.println(ng.solve(0, n - 1));
-    }
-
-    private int solve(int i, int j) {
-        if (i > j) {
-            return 0;
-        }
-
-        if (score[i][j] != 0) {
-            return score[i][j];
-        }
-
-        if (i == j) {
-            return score[i][j] = seq[i];
-        }
-
-        final int p2ls = solve(i + 1, j);
-        final int p2rs = solve(i, j - 1);
-
-        return score[i][j] = sum[i][j] - Math.min(p2ls, p2rs);
     }
 }
