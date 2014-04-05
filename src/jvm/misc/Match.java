987f42184c683c23a8642b1c08d72bbf7af62e3f
rdsr <rdsr@yahoo-inc.com>
2014-04-05 10:42:42 +0530

    regex code

--- src/jvm/misc/Match.java
+++ src/jvm/misc/Match.java
@@ -1,25 +1,49 @@
 package misc;
 
 public class Match {
-  boolean matches(String s, String re) {
-    if (s == null)
-      throw new NullPointerException("input string is null");
-    
-    if (s.length() == 0)
-      if (re.length() == 0 || (re.length() == 1 && re.charAt(0) == '*'))
-        return true;
-      else
-        return false;
-    
-    if (re.charAt(0) == '?' || s.charAt(0) == re.charAt(0))
-      return matches(s.substring(1), re.substring(1));
-    
-    boolean matched = false;
-    if (re.charAt(0) == '*') {
-      for (int i = 0; i < s.length() && !matched; i++) {
-        matched = matches(s.substring(i), re.substring(1));
-      }
+    static boolean matches(String s, String re) {
+        if (s == null) {
+            throw new NullPointerException("input string is null");
+        }
+
+        if (re.equals("*")) {
+            return true;
+        }
+
+        if (s.length() == 0) {
+            if (re.length() == 0) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        if (re.charAt(0) == '?' || s.charAt(0) == re.charAt(0)) {
+            return matches(s.substring(1), re.substring(1));
+        }
+
+        boolean matched = false;
+        if (re.charAt(0) == '*') {
+            for (int i = 0; i < s.length() && !matched; i++) {
+                matched = matches(s.substring(i), re.substring(1));
+            }
+        }
+        return matched;
     }
-    return matched; 
-  }
+
+    public static void main(String[] args) {
+        //false 
+        System.out.println(Match.matches("", "?*"));
+        System.out.println(Match.matches("", "?"));
+        System.out.println(Match.matches("", "*?"));
+        
+        //true
+        System.out.println(Match.matches("a", "?"));
+        System.out.println(Match.matches("", "*"));
+        System.out.println(Match.matches("abc", "*c"));
+        System.out.println(Match.matches("abc", "*bc"));
+        System.out.println(Match.matches("abc", "*abc"));
+        System.out.println(Match.matches("abc", "???"));
+        System.out.println(Match.matches("abc", "*"));
+    }
 }
